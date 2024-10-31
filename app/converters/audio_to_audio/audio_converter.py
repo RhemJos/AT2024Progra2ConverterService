2a062c3fedadcb72e1c861269169f6c79cdff8e0
yujrajo <joanna.yujra@jala.university>
2024-10-31 19:23:08 -0400

    Audio converter feature, implemented with ffmpeg-python

---
+++ app/converters/audio_to_audio/audio_converter.py
@@ -0,0 +1,39 @@
+import os
+import ffmpeg
+
+class AudioConverter():
+    def __init__(self,audio_path):
+        self.audio_path=audio_path
+
+    def convert(self, output_format='mp3', **kwargs):
+        output_file = os.path.splitext(os.path.basename(self.audio_path))[0] +'.' + output_format
+        output_path = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())),'outputs','audio_converted_outputs',output_file)
+        # si el archivo ya existe no convertir
+        if os.path.exists(output_path):
+            print(f'El archivo {output_path} ya existe.')
+            return output_path
+        # setear opciones
+        options= {'vn':None}
+        if 'bit_rate' in kwargs:
+            options['b:a']= kwargs['bit_rate']
+        if 'channels' in kwargs:
+            options['ac']= kwargs['channels']
+        if 'sample_rate' in kwargs:
+            options['ar']= kwargs['sample_rate']
+        if 'volume' in kwargs:
+            options['af']= f"volume={kwargs['volume']}"
+
+        try:
+            stdout,stderr=(
+                ffmpeg
+                .input(self.audio_path)
+                .output(output_path,
+                        **options
+                        )
+                # .run(overwrite_output=True)
+                .run(capture_stdout=True,capture_stderr=True)
+            )
+        except ffmpeg.Error as e:
+            print(f"Algo falló en la conversión : {e.stderr.decode()}")
+        else:
+            return output_path
