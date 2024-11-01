2a13a74f737852b750951e31f0347ca20ab1534a
Jessica Orihuela Rojas <jessica.orihuela.rojas@gmail.com>
2024-11-01 07:30:49 -0400

    Editing MetadataExtractor class

--- converters/extractor/metadataextractor.py
+++ converters/extractor/metadataextractor.py
@@ -1,15 +1,23 @@
 import platform
+import os
 from utils import CommandExecutor, Formatter
 from converters.extractor.extractor import Extractor
 
-
 class MetadataExtractor(Extractor):
     def extract(self):
-        exif_tool_distribution = "Windows" if platform.system() == "Windows" else "Perl" 
-        cmd = CommandExecutor()
-        exif_tool_path = cmd.get_execfile_path("\\converters\\extractor\\bin\\exifTool\\exifTool{}".format(exif_tool_distribution) )
-        exif_tool_command = "{}\\exiftool {}".format(exif_tool_path, self.file_path) 
+        
+        exif_tool_distribution = "exifTool/exifToolWindows/exiftool.exe" if platform.system() == "Windows" else "exifToolPerl/exiftool" # Determinar el nombre del archivo según el sistema operativo
+        base_dir = os.path.dirname(os.path.abspath(__file__))# Obtener la ruta base del archivo actual
+        exif_tool_path = os.path.join(base_dir, "bin", exif_tool_distribution).replace("\\", "/") #Construir la ruta absoluta del ejecutable exiftool
+        
+        if not os.path.isfile(exif_tool_path):
+            return {"error": f"No se encontró el archivo exiftool en la ruta: {exif_tool_path}"}
+        
+        exif_tool_command = f"{exif_tool_path} {self.file_path}" #Crear el comando para ejecutar exiftool
+        
+        cmd = CommandExecutor() # Ejecutar el comando
         result_text = cmd.run_command(exif_tool_command)
-        formatter = Formatter()
+        
+        formatter = Formatter() # Formatear el resultado a diccionario
         return formatter.key_value_string_to_dict(result_text, ":", "\n")
 
