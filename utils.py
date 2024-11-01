2f63e6d21da03ad6f039fa674adcdf27d75b5e91
michelle-gh <michelle.hernandez@correounivalle.edu.co>
2024-11-01 12:36:36 -0500

    Refactor code for obtaining absolute file path. Add file name check

--- utils.py
+++ utils.py
@@ -1,17 +1,22 @@
 import subprocess
 import os
+import errno
 
 
 class CommandExecutor:
     def get_execfile_path(self, relative_path):
-        return os.getcwd() + relative_path
+        base_dir = os.getcwd() #Obtiene la ruta base del proyecto
+        absolute_file_path = os.path.join( base_dir, relative_path)
+        if not os.path.isfile(absolute_file_path):
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), absolute_file_path) #Levanta una excepcion en caso de no hallar el archivo
+        return  absolute_file_path
 
     def run_command(self, command):
         try:
             return subprocess.run(command, shell=True, check=True, 
                                   encoding='utf-8', capture_output=True).stdout
         except subprocess.CalledProcessError:
-            return "No se pudo ejecutar el comando"
+            raise
         
 class Formatter:
     # Formats a string containing key:value pairs into a dictionary
