e0130e1141403bc9178368b6444292a561719ddb
Jessica Orihuela Rojas <jessica.orihuela.rojas@gmail.com>
2024-11-01 04:43:59 -0400

    Edit ImageConverter class and '/image-configuration' endpoint

--- app/converters/image_to_image/image_converter.py
+++ app/converters/image_to_image/image_converter.py
@@ -2,54 +2,48 @@ import string
 from PIL import Image
 from PIL.ImagePalette import random
 import random
+import os
 
 class ImageConverter:
     def __init__(self, path, extension):
         self.path = path
         self.extension = extension
 
-    def image_resize (self, path):
-        img = Image.open(path)
-        img_resized = img.resize((int(img.width/2), int(img.height/2)), box=(20,20,300,300))
-        name = self.generate_name()
-        img_resized.save('Resize/'+name+'.png')
-        file_path = 'Resize/'+name+'.png'
-        return file_path
+    def image_resize(self, width=None, height=None):
+        img = Image.open(self.path)
+        if width is None or height is None:
+            width = img.width
+            height = img.height
+        img_resized = img.resize((width, height))
+        return img_resized
 
-    def image_rotate (self, path):
-        img = Image.open(path)
-        img_rotated = img.rotate(angle=60, expand=True, fillcolor="black") #center=(0,0)
-        name = self.generate_name()
-        img_rotated.save('Rotate/'+name+'.png')
-        file_path = 'Rotate/'+name+'.png'
-        return file_path
+    def image_rotate(self, img, angle):
+        img_rotated = img.rotate(angle, expand=True, fillcolor="black")
+        return img_rotated
 
-    def image_grayscale (self, path):
-        img = Image.open(path)
-        img_grayscaled = img.convert("L")
-        name = self.generate_name()
-        img_grayscaled.save('Grayscale/'+name+'.png')
-        file_path = 'Grayscale/'+name+'.png'
-        return file_path
+    def image_grayscale(self, img, apply):
+        if apply:
+            img_grayscaled = img.convert("L")
+            return img_grayscaled
+        return img
 
-    def image_convert (self):
-        resized = self.image_resize(self.path)
-        rotated = self.image_rotate(resized)
-        grayscale = self.image_grayscale(rotated)
-        name = self.generate_name()
-        img_converted = Image.open(grayscale)
-        print(self.extension)
-        img_converted.convert("RGB")
-        img_converted.save('../../outputs/image_converted_outputs/'+name+'.'+self.extension, format = self.extension)
-        output_path = '../../outputs/image_converted_outputs/'+name+'.'+self.extension
-        print('Image converted successfully '+ output_path)
+    def image_convert(self, resize=None, rotate=None, grayscale=None):
+        img = self.image_resize()
+
+        if rotate is not None:
+            img = self.image_rotate(img, rotate)
+        
+        img = self.image_grayscale(img, grayscale)
+
+        base_name = os.path.splitext(os.path.basename(self.path))[0]
+        edited_name = f"{base_name}_edited.{self.extension}"
+        output_path = os.path.join('app', 'outputs', 'image_converted_outputs', edited_name)
+        img.save(output_path)
+
         return output_path
-
-
-    @staticmethod
-    def generate_name ():
-        chars = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase
-        name = ""
-        for i in range (10):
-            name += random.choice(chars)
-        return name
+    
+    # @staticmethod
+    # def generate_name():
+    #     chars = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase
+    #     name = "".join(random.choice(chars) for _ in range(10))
+    #     return name
