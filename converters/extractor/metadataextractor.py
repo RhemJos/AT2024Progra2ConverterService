a0e0b30588f79fa47419a60c181b9c1fae8290ea
michelle-gh <michelle.hernandez@correounivalle.edu.co>
2024-11-01 12:36:36 -0500

    Refactor code for obtaining absolute file path. Add file name check

--- converters/extractor/metadataextractor.py
+++ converters/extractor/metadataextractor.py
@@ -3,19 +3,17 @@ import os
 from utils import CommandExecutor, Formatter
 from converters.extractor.extractor import Extractor
 
+
 class MetadataExtractor(Extractor):
     def extract(self):
-        
-        exif_tool_distribution = "exifTool/exifToolWindows/exiftool.exe" if platform.system() == "Windows" else "exifToolPerl/exiftool" # Determinar el nombre del archivo según el sistema operativo
-        base_dir = os.path.dirname(os.path.abspath(__file__))# Obtener la ruta base del archivo actual
-        exif_tool_path = os.path.join(base_dir, "bin", exif_tool_distribution).replace("\\", "/") #Construir la ruta absoluta del ejecutable exiftool
-        
-        if not os.path.isfile(exif_tool_path):
-            return {"error": f"No se encontró el archivo exiftool en la ruta: {exif_tool_path}"}
-        
-        exif_tool_command = f"{exif_tool_path} {self.file_path}" #Crear el comando para ejecutar exiftool
-        
         cmd = CommandExecutor() # Ejecutar el comando
+
+        if " " in os.path.basename(self.file_path): #Si el nombre del archivo tiene espacios lo pone entre comillas
+            self.file_path = '"' + self.file_path + '"'
+
+        exif_tool_distribution = "exifToolWindows/exiftool.exe" if platform.system() == "Windows" else "exifToolPerl/exiftool" # Determinar el nombre del archivo según el sistema operativo
+        exif_tool_path = cmd.get_execfile_path( "converters/extractor/bin/exifTool/" + exif_tool_distribution) #Construir la ruta absoluta del ejecutable exiftool
+        exif_tool_command = f"{exif_tool_path} {self.file_path}" #Crear el comando para ejecutar exiftool
         result_text = cmd.run_command(exif_tool_command)
         
         formatter = Formatter() # Formatear el resultado a diccionario
