864413d0308c587147bc19267e988ae80c67330d
Jessica Orihuela Rojas <jessica.orihuela.rojas@gmail.com>
2024-11-07 15:45:27 -0400

    Edit video-converter

--- converters/video_to_images/video_converter.py
+++ converters/video_to_images/video_converter.py
@@ -24,13 +24,22 @@ class VideoConverter:
 
     def convert_to_format(self, output_format=None, fps=None, video_codec=None, audio_codec=None, audio_channels=None): 
         filename = os.path.splitext(os.path.basename(self.video_path))[0]
+        input_format = os.path.splitext(self.video_path)[1][1:].lower()
+
+        temp_output_path = os.path.join('outputs', 'video_converted_output', f"{filename}-converted.{output_format}") # path temporal
         output_path = os.path.join('outputs', 'video_converted_output', f"{filename}.{output_format}")
 
         ffmpeg_command = ffmpeg.input(self.video_path).output(
-            output_path, 
-            **({'vcodec': video_codec} if video_codec else {}), 
-            **({'acodec': audio_codec} if audio_codec else {}), 
-            **({'ac': audio_channels} if audio_channels else {}), 
+            temp_output_path if input_format == output_format else output_path,
+            **({'vcodec': video_codec} if video_codec else {}),
+            **({'acodec': audio_codec} if audio_codec else {}),
+            **({'ac': audio_channels} if audio_channels else {}),
             **({'r': fps} if fps else {})
         )
+
         ffmpeg_command.run(overwrite_output=True)
+
+        if input_format == output_format:
+            if os.path.exists(output_path):
+                os.remove(output_path)
+            os.rename(temp_output_path, output_path)
