921cc50c3d491543c977da40723b3b9192c7a356
Daria <reillyfernandez17@gmail.com>
2024-11-08 15:21:52 -0400

    Change comments to English

--- converters/audio_to_audio/audio_converter.py
+++ converters/audio_to_audio/audio_converter.py
@@ -12,12 +12,12 @@ class AudioConverter:
     def convert(self, output_format='mp3', **kwargs):
         output_path = self._get_output_path(output_format)
 
-        # si el archivo ya existe no convertir
+        # If the file already exists do not convert
         if os.path.exists(output_path):
             print(f'El archivo {output_path} ya existe.')
             return output_path
 
-        # setear opciones - cambiar a metodo
+        # Set options
         options = self._get_audio_options(**kwargs)
 
         try:
@@ -28,15 +28,16 @@ class AudioConverter:
             return output_path
 
     def _get_output_path(self, output_format):
-        # Genera la ruta de salida para el archivo convertido y cambia formato
+        # Generate output path for the converted file and change format
         output_file = os.path.splitext(os.path.basename(self.audio_path))[0] + '.' + output_format
         return os.path.join('outputs', 'audio_converted_outputs', output_file)
 
     def _convert_audio(self, output_path, options):
-        # Realiza la conversión de audio utilizando ffmpeg
+        # Performs audio conversion using ffmpeg
         ffmpeg.input(self.audio_path).output(output_path, **options).run(capture_stdout=True, capture_stderr=True)
 
     def _get_audio_options(self, **kwargs):
+        # Verifies that options have been built
         opt = AudioOptions.build_options_audio(**kwargs)
         if opt is None:
             raise AudioConversionError(f"Error al construir opciones de audio")
