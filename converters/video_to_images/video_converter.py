946152cdac38b2c3ac0e0b3209753a7f93129073
michelle-gh <michelle.hernandez@correounivalle.edu.co>
2024-11-07 20:15:36 -0500

    Resizing based on preset formats. Handle exceptions. Abstract converter

--- converters/video_to_images/video_converter.py
+++ converters/video_to_images/video_converter.py
@@ -1,12 +1,10 @@
 import os
 import ffmpeg
+from converters.converter import Converter
 
-class VideoConverter:
-    def __init__(self, video_path):
-        self.video_path = video_path
-
+class VideoConverter(Converter):
     def to_frames(self, output_path=None, fps=1):
-        filename = os.path.splitext(os.path.basename(self.video_path))[0] #Obtiene el nombre del video
+        filename = os.path.splitext(os.path.basename(self.file_path))[0] #Obtiene el nombre del video
         frames_folder = os.path.join('outputs', 'video_to_frames_output', filename)
         os.makedirs(frames_folder, exist_ok=True)
 
@@ -15,7 +13,7 @@ class VideoConverter:
         
         (
             ffmpeg
-            .input(self.video_path)
+            .input(self.file_path)
             .filter('fps', fps=fps)
             .output(output_path)
             .run(overwrite_output=True)
@@ -24,7 +22,7 @@ class VideoConverter:
 
 
     def convert_format(self, output_format=None, output_path=None):
-        base_name = os.path.basename(self.video_path)  # Obtiene el nombre base del video con la extensión E.g: .mp4
+        base_name = os.path.basename(self.file_path)  # Obtiene el nombre base del video con la extensión E.g: .mp4
         file_name = os.path.splitext(base_name)[0]      #Obtiene el nombre del video sin la extensión
         video_path = f'{file_name}.{output_format}'   #Añadiendo nueva extension de video al nombre del video original del input
         video_converted_folder = os.path.join('outputs', 'video_converted_output')
@@ -35,7 +33,7 @@ class VideoConverter:
         print('output_path: ', output_path)
         (
             ffmpeg
-            .input(self.video_path)
+            .input(self.file_path)
             .output(output_path)
             .run(overwrite_output=True)
         )
