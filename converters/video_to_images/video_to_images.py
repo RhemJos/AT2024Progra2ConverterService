9e6f3bee8b331400f981da89966ca8fbe526a43a
Jessica Orihuela Rojas <jessica.orihuela.rojas@gmail.com>
2024-11-12 00:46:52 -0400

    Add **kwargs in VideoToImagesConverter convert method

--- converters/video_to_images/video_to_images.py
+++ converters/video_to_images/video_to_images.py
@@ -7,14 +7,23 @@ class VideoToImagesConverter(Converter):
     def __init__(self, video_path):
         super().__init__(video_path)
 
-    def convert(self, output_path=None, fps=1):
-        # Convierte el video en una serie de fotogramas
+    def convert(self, output_path=None, fps=1, **kwargs):
         frames_folder = os.path.join('outputs', 'video_to_frames_outputs', self.filename)
         os.makedirs(frames_folder, exist_ok=True)
 
         if output_path is None:
             output_path = os.path.join(frames_folder, '%d.jpg')
 
-        # Comando ffmpeg para extraer los fotogramas del video
-        ffmpeg.input(self.file_path).filter('fps', fps=fps).output(output_path).run(overwrite_output=True)
-        return frames_folder
+        ffmpeg_command = ffmpeg.input(self.file_path)
+
+        if fps:
+            ffmpeg_command = ffmpeg_command.filter('fps', fps=fps)
+
+        ffmpeg_command = ffmpeg_command.output(output_path, **kwargs)
+
+        try:
+            ffmpeg_command.run(overwrite_output=True)
+            return frames_folder
+        except ffmpeg.Error as e:
+            print(f"Error al ejecutar el comando ffmpeg: {e.stderr.decode('utf8')}")
+            raise
