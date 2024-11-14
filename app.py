0eeb435dd118bfef8d9e1eed3bf52d5c11436a10
RhemHuarachi <rhemjos@gmail.com>
2024-11-14 00:07:57 -0400

    Update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #
-# @app.py Copyright (c) 2021 Jalasoft. # 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia. # <add direccion de jala la paz>
+# @app.py Copyright (c) 2021 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia. # <add direccion de jala la paz>
 # All rights reserved. #
 # This software is the confidential and proprietary information of
 # Jalasoft, ("Confidential Information"). You shall not
@@ -10,8 +11,6 @@
 
 from flask import Flask
 from models import db
-#from routes import test_route
-from api.endpoints import api
 from os import environ
 
 from routes.metadata_routes import metadata_blueprint
@@ -19,6 +18,7 @@ from routes.audio_routes import audio_blueprint
 from routes.image_routes import image_blueprint
 from routes.video_routes import video_blueprint
 from routes.download_routes import download_blueprint
+from routes.login_routes import login_blueprint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
@@ -28,14 +28,13 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-# app.register_blueprint(test_route)
-app.register_blueprint(api, url_prefix='/api')
-
 app.register_blueprint(metadata_blueprint, url_prefix='/api')
 app.register_blueprint(audio_blueprint, url_prefix='/api')
 app.register_blueprint(image_blueprint, url_prefix='/api')
 app.register_blueprint(video_blueprint, url_prefix='/api')
 app.register_blueprint(download_blueprint, url_prefix='/api')
+app.register_blueprint(login_blueprint, url_prefix='/api')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9090, debug=True)
