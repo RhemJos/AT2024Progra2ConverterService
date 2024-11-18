3a836737ebbca77dae149948c6ab25f1d58130b6
yujrajo <joanna.yujra@jala.university>
2024-11-18 14:51:38 -0400

    Add unit tests for video_to_video and audio_to_audio converters. Plus their respective media files

---
+++ tests/converters/audio_to_audio/test_audio_converter.py
@@ -0,0 +1,252 @@
+import unittest
+import os
+from converters.audio_to_audio.audio_converter import AudioConverter
+from converters.audio_to_audio.audio_exception import AudioConversionError
+
+
+class TestAudioConverter(unittest.TestCase):
+
+    # define output_path to remove it at the end of tests
+    def setUp(self):
+        self.output_path = ""
+
+    # remove converted_audios
+    def tearDown(self):
+        if os.path.exists(self.output_path):
+            os.remove(self.output_path)
+
+    # Positive, with valid audio_path and output_format and all kwargs
+    def test_audio_converter_success(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        self.output_path = converter.convert(
+            output_format="m4a",
+            bit_rate="32k",
+            channels=2,
+            sample_rate=8000,
+            volume=1.5,
+            language_channel=0,
+            speed=1.2
+        )
+        expected = os.path.join("outputs", "audio_converted_outputs", "audio.m4a")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid audio_path and output_format no kwargs
+    def test_audio_converter_valid_audio_path_and_format_no_kwargs(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        self.output_path = converter.convert(
+            output_format="m4a",
+        )
+        expected = os.path.join("outputs", "audio_converted_outputs", "audio.m4a")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid audio_path and output_format and bit_rate
+    def test_audio_converter_valid_audio_path_and_format_and_bit_rate(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        self.output_path = converter.convert(
+            output_format="m4a",
+            bit_rate="32k"
+        )
+        expected = os.path.join("outputs", "audio_converted_outputs", "audio.m4a")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid audio_path and output_format and channels
+    def test_audio_converter_valid_audio_path_and_format_and_channels(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        self.output_path = converter.convert(
+            output_format="m4a",
+            channels=2
+        )
+        expected = os.path.join("outputs", "audio_converted_outputs", "audio.m4a")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid audio_path and output_format and sample_rate
+    def test_audio_converter_valid_audio_path_and_format_and_sample_rate(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        self.output_path = converter.convert(
+            output_format="m4a",
+            sample_rate=8000
+        )
+        expected = os.path.join("outputs", "audio_converted_outputs", "audio.m4a")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid audio_path and output_format and volume
+    def test_audio_converter_valid_audio_path_and_format_and_volume(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        self.output_path = converter.convert(
+            output_format="m4a",
+            volume=1.5
+        )
+        expected = os.path.join("outputs", "audio_converted_outputs", "audio.m4a")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid audio_path and output_format and language_channel
+    def test_audio_converter_valid_audio_path_and_format_and_language_channel(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        self.output_path = converter.convert(
+            output_format="m4a",
+            language_channel=0
+        )
+        expected = os.path.join("outputs", "audio_converted_outputs", "audio.m4a")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid audio_path and output_format and speed
+    def test_audio_converter_valid_audio_path_and_format_and_speed(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        self.output_path = converter.convert(
+            output_format="m4a",
+            speed=1.2
+        )
+        expected = os.path.join("outputs", "audio_converted_outputs", "audio.m4a")
+        self.assertEqual(self.output_path, expected)
+
+    # Negative, with audio_path as None
+    def test_audio_converter_invalid_audio_path_None(self):
+        audio_path = None
+        with self.assertRaises(TypeError):
+            converter = AudioConverter(audio_path)
+
+    # Negative, with audio_path as empty string
+    def test_audio_converter_invalid_audio_path_empty(self):
+        audio_path = ""
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                output_format="m4a"
+            )
+
+    # Negative, with audio_path that does not exist
+    def test_audio_converter_audio_path_does_not_exist(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "does_not_exist.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                output_format="m4a"
+            )
+
+    # Negative, with audio_path which extension was changed
+    # jpg --> wav, image does not contain audio and an exception is raised
+    def test_audio_converter_audio_path_image_extension_changed(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "JPG.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                output_format="m4a"
+            )
+
+    #  --ERROR-- Negative, with audio_path which extension was changed
+    #  mp4 --> wav, video contains audio and gets recognized as audio, but it should not
+    def test_audio_converter_audio_path_video_extension_changed(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "MP4.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                output_format="m4a"
+            )
+
+    # Negative, with audio_path which extension was changed - files
+    def test_audio_converter_audio_path_files_extension_changed(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "PDF.wav")
+        # audio_path = os.path.join("tests", "converters", "audio_to_audio", "outputs","audio_converted_outputs","text.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                output_format="m4a"
+            )
+
+    # Negative, with audio_path corrupted
+    def test_audio_converter_audio_path_corrupted(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "corrupt.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                output_format="m4a"
+            )
+
+    # --ERROR-- BECAUSE CONVERSION COMPLETED, FORMAT = MP3 BY DEFAULT-- SHOULD WE CHANGE IT?
+    # Negative, without parameters
+    def test_audio_converter_without_parameters(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert()
+
+    # Negative, with invalid output_format
+    def test_audio_converter_with_invalid_output_format(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                output_format="invalid_format"
+            )
+
+    # Negative, with invalid bit_rate
+    def test_audio_converter_with_invalid_bit_rate(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                bit_rate="invalid_bit_rate"
+            )
+
+    # Negative, with invalid channels
+    def test_audio_converter_with_invalid_channels(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                channels="invalid_channels"
+            )
+
+    # Negative, with invalid sample_rate
+    def test_audio_converter_with_invalid_sample_rate(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                sample_rate="invalid_sample_rate"
+            )
+
+    # Negative, with invalid volume
+    def test_audio_converter_with_invalid_volume(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                volume="invalid_volume"
+            )
+
+    # Negative, with invalid language_channel
+    def test_audio_converter_with_invalid_language_channel(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                language_channel="invalid_language_channel"
+            )
+
+    # --ERROR-- ValueError instead of AudioConversionError because is not a number and float() fails
+    # Negative, with invalid speed
+    def test_audio_converter_with_invalid_speed(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                speed="invalid_speed"
+            )
+
+    # Negative, with invalid speed number
+    def test_audio_converter_with_invalid_speed_number(self):
+        audio_path = os.path.join("tests", "converters", "audio_to_audio", "audio.wav")
+        converter = AudioConverter(audio_path)
+        with self.assertRaises(AudioConversionError):
+            self.output_path = converter.convert(
+                speed=5
+            )
---
+++ tests/converters/video_to_video/test_video_to_video.py
@@ -0,0 +1,207 @@
+import unittest
+import ffmpeg
+import os
+from converters.video_to_video.video_to_video import VideoToVideoConverter
+
+
+class TestVideoToVideoConverter(unittest.TestCase):
+
+    # define output_path to remove it at the end of tests
+    def setUp(self):
+        self.output_path = ""
+
+    # def tearDown(self):
+    #     # remove converted_videos
+    #     if os.path.exists(self.output_path):
+    #         os.remove(self.output_path)
+
+    # Positive, with valid video_path and output_format and all kwargs
+    def test_video_converter_success(self):
+        # video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        video_path = os.path.join("tests","converters", "video_to_video","video.mp4")
+        # video_path = r".\tests\converters\video_to_video\video.mp4"
+        converter = VideoToVideoConverter(video_path)
+        self.output_path = converter.convert(
+            output_format="avi",
+            fps=24,
+            video_codec="libx265",
+            audio_codec="mp3",
+            audio_channels=2,
+        )
+        expected = os.path.join("outputs", "video_to_video_outputs", "video-converted.avi")
+        # expected = os.path.join("tests", "converters", "video_to_video","outputs", "video_to_video_outputs", "video-converted.avi")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid video_path and output_format no kwargs
+    def test_video_converter_valid_video_path_and_format_no_kwargs(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        self.output_path = converter.convert(
+            output_format="avi",
+        )
+        expected = os.path.join("outputs", "video_to_video_outputs", "video-converted.avi")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid video_path and output_format and fps
+    def test_video_converter_valid_video_path_and_format_and_fps(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        self.output_path = converter.convert(
+            output_format="avi",
+            fps=24,
+        )
+        expected = os.path.join("outputs", "video_to_video_outputs", "video-converted.avi")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid video_path and output_format and video_codec
+    def test_video_converter_valid_video_path_and_format_and_video_codec(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        self.output_path = converter.convert(
+            output_format="avi",
+            video_codec="libx265",
+        )
+        expected = os.path.join("outputs", "video_to_video_outputs", "video-converted.avi")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid video_path and output_format and audio_codec
+    def test_video_converter_valid_video_path_and_format_and_audio_codec(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        self.output_path = converter.convert(
+            output_format="avi",
+            audio_codec="mp3",
+        )
+        expected = os.path.join("outputs", "video_to_video_outputs", "video-converted.avi")
+        self.assertEqual(self.output_path, expected)
+
+    # Positive, with valid video_path and output_format and audio_channels
+    def test_video_converter_valid_video_path_and_format_and_audio_channels(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        self.output_path = converter.convert(
+            output_format="avi",
+            audio_channels=2,
+        )
+        expected = os.path.join("outputs", "video_to_video_outputs", "video-converted.avi")
+        self.assertEqual(self.output_path, expected)
+
+    # Negative, with video_path as None
+    def test_video_converter_invalid_input_None(self):
+        video_path = None
+        with self.assertRaises(TypeError):
+            converter = VideoToVideoConverter(video_path)
+
+    # Negative, with video_path as empty string
+    def test_video_converter_invalid_input_empty(self):
+        video_path = ""
+        converter = VideoToVideoConverter(video_path)
+        with self.assertRaises(ffmpeg.Error):
+            self.output_path = converter.convert(
+                output_format="avi",
+            )
+
+    # Negative, with video_path that does not exist
+    def test_video_converter_video_path_does_not_exist(self):
+        video_path = os.path.join( "does_not_exist.mp4")
+        converter = VideoToVideoConverter(video_path)
+        with self.assertRaises(ffmpeg.Error):
+            self.output_path = converter.convert(
+                output_format="avi",
+            )
+
+    # --ERROR-- Negative, with video_path which extension was changed - audio files
+    #  ogg --> mp4, the conversion worked and created a video with only audio, but it should not
+    def test_video_converter_input_audio_file_extension_changed(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "OGG.mp4")
+        with self.assertRaises(Exception):
+            converter = VideoToVideoConverter(video_path)
+            self.output_path = converter.convert(
+                output_format="avi",
+            )
+
+    # --ERROR-- Negative, with video_path which extension was changed - image files
+    #  jpg --> mp4, the conversion worked and created a video with only an image, but it should not
+    def test_video_converter_input_image_file_extension_changed(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "JPG.mp4")
+        with self.assertRaises(Exception):
+            converter = VideoToVideoConverter(video_path)
+            self.output_path = converter.convert(
+                output_format="avi",
+            )
+
+    # Negative, with video_path which extension was changed - files
+    def test_video_converter_input_files_extension_changed(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "PDF.mp4")
+        # video_path = os.path.join("tests", "converters", "video_to_video","text.mp4")
+        with self.assertRaises(ffmpeg.Error):
+            converter = VideoToVideoConverter(video_path)
+            self.output_path = converter.convert(
+                output_format="avi",
+            )
+
+    # Negative, with video_path corrupted
+    def test_video_converter_video_corrupted(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "corrupt.mp4")
+        with self.assertRaises(ffmpeg.Error):
+            converter = VideoToVideoConverter(video_path)
+            self.output_path = converter.convert(
+                output_format="avi",
+            )
+
+    # Negative, without parameters
+    def test_video_converter_without_parameters(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        with self.assertRaises(ffmpeg.Error):
+            self.output_path = converter.convert()
+
+    # Negative, with invalid format
+    def test_video_converter_with_invalid_output_format(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        with self.assertRaises(ffmpeg.Error):
+            self.output_path = converter.convert(
+                output_format="invalid_format"
+            )
+
+    # Negative, with invalid fps
+    def test_video_converter_with_invalid_fps(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        with self.assertRaises(ValueError):
+            self.output_path = converter.convert(
+                output_format="avi",
+                fps="invalid_fps"
+            )
+
+    # Negative, with invalid video codec
+    def test_video_converter_with_invalid_video_codec(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        with self.assertRaises(ffmpeg.Error):
+            self.output_path = converter.convert(
+                output_format="avi",
+                video_codec="invalid_video_codec"
+            )
+
+    # --ERROR-- converts file and audio:0KiB, but it should not
+    # Negative, with invalid audio codec
+    def test_video_converter_with_invalid_audio_codec(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        with self.assertRaises(ffmpeg.Error):
+            self.output_path = converter.convert(
+                output_format="avi",
+                audio_codec="invalid_audio_codec"
+            )
+
+    # Negative, with invalid audio channels
+    def test_video_converter_with_invalid_audio_channels(self):
+        video_path = os.path.join("tests", "converters", "video_to_video", "video.mp4")
+        converter = VideoToVideoConverter(video_path)
+        with self.assertRaises(ValueError):
+            self.output_path = converter.convert(
+                output_format="avi",
+                audio_channels="invalid_audio_channels"
+            )
